64fdfbe2cf1104138c463bf88ac7a4d95706cc38
n.kozlov <nikitacauselove@gmail.com>
2023-12-08 00:50:14 +0300

    refactor: удалить лишние аннотации

--- week-2/src/main/java/com/aston/trainee/entity/Author.java
+++ week-2/src/main/java/com/aston/trainee/entity/Author.java
@@ -1,11 +1,9 @@
 package com.aston.trainee.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @NoArgsConstructor
 @Setter
